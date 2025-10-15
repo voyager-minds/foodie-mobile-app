73c1e6a939bf7af031b6ea3b8e9e1932bdde06ec
Dilshika Bandara <30935988+Dilshika@users.noreply.github.com>
2025-10-15 11:54:06 +0530

    update styles

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,15 @@ import { Ionicons } from '@expo/vector-icons';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useRouter } from 'expo-router';
 import { useEffect, useRef } from 'react';
-import {
-	Animated,
-	Dimensions,
-	StatusBar,
-	Text,
-	TouchableOpacity,
-	View,
-} from 'react-native';
+import
+	{
+		Animated,
+		Dimensions,
+		StatusBar,
+		Text,
+		TouchableOpacity,
+		View,
+	} from 'react-native';
 
 import { useTheme } from '@/store/useTheme';
 import { indexStyles } from './styles';
